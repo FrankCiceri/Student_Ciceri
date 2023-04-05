0d970a487994148780a186587ab094dce7bdd079
FrankCiceri <frankanthonyc00@gmail.com>
2023-04-05 15:45:12 -0500

    added change to hello_ciceri.cs

--- hello_ciceri.cs
+++ hello_ciceri.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!, this is file1, goodbye Frank");
+Console.WriteLine("Hello, World!, this is file1, goodbye Frank, change on V1");
